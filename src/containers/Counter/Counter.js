69f4caa7a8ebc027883feebd97c3a38e19999a11
sunilale0 <sunilale0@gmail.com>
2019-08-14 00:43:38 -0400

    finished counter with redux

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,27 +1,79 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actionTypes from "../../store/actions";
 
-
-class Counter extends Component{
-
-    render() {
-        return (<div>
-             {this.props.ctr}
+class Counter extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div>{this.props.ctr}</div>
+        <div>
+          <button
+            onClick={this.props.onIncrementCounter}
+            className="btn btn-primary"
+          >
+            Increment
+          </button>
+          <button
+            onClick={this.props.onDecrementCounter}
+            className="btn btn-danger"
+          >
+            Decrement
+          </button>
+          <button onClick={this.props.onAddCounter} className="btn btn-primary">
+            Add 10
+          </button>
+          <button
+            onClick={this.props.onSubtractCounter}
+            className="btn btn-danger"
+          >
+            Subtract 8
+          </button>
+          <hr />
+          <button
+            onClick={() => this.props.onStoreResult(this.props.ctr)}
+            className="btn btn-success btn-block"
+          >
+            {" "}
+            Store Result
+          </button>
+          <ul>
+            {this.props.storedResults.map(strResult => (
+              <li
+                key={strResult.id}
+                onClick={() => this.props.onDeleteResult(strResult.id)}
+              >
+                {strResult.value}
+              </li>
+            ))}
+          </ul>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        ctr: state.counter
-    }
-}
+  return {
+    ctr: state.ctr.counter,
+    storedResults: state.res.results
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onIncrementCounter: ()=> dispatch({type: 'INCREMENT'})
-    }
-}
+  return {
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: () => dispatch({ type: actionTypes.ADD, value: 10 }),
+    onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, value: 8 }),
+    onStoreResult: result =>
+      dispatch({ type: actionTypes.STORE_RESULT, result: result }),
+    onDeleteResult: id =>
+      dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id })
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
---
+++ src/store/reducers/result.js
@@ -0,0 +1,37 @@
+import * as actionTypes from "../actions";
+
+const initialState = {
+  results: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: action.result })
+      };
+    case actionTypes.DELETE_RESULT:
+      //   const id = 2;
+      //   // here, elements if they are object, the objects themselves
+      //   // are pointing the same object, but deleting is okay
+      //   const newArray = [...state.results];
+      //   state.results.splice(id, 1);
+      //   return {
+      //     ...state,
+      //     results: newArray
+      //   };
+      console.log("Delete Result fired!");
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray
+      };
+  }
+
+  return state;
+};
+
+export default reducer;
