424ca04630fc8b54b4912beaa5c00d35aa768972
sunilale0 <sunilale0@gmail.com>
2019-08-30 22:59:18 -0400

    finished

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   counter: 0
@@ -11,24 +12,16 @@ const reducer = (state = initialState, action) => {
       //     ...state,
       //     counter: state.counter + 1
       //   };
-      const newState = Object.assign({}, state);
-      newState.counter = state.counter + 1;
-      return newState;
+      return updateObject(state, { counter: state.counter + 1 });
+
     case actionTypes.DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1
-      };
+      return updateObject(state, { counter: state.counter - 1 });
+
     case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value
-      };
+      return updateObject(state, { counter: state.counter + action.value });
+
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.value
-      };
+      return updateObject(state, { counter: state.counter - action.value });
   }
 
   return state;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,38 +1,42 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   results: []
+};
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    result => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       // change data
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result })
-      };
+      });
     case actionTypes.DELETE_RESULT:
-      //   const id = 2;
-      //   // here, elements if they are object, the objects themselves
-      //   // are pointing the same object, but deleting is okay
-      //   const newArray = [...state.results];
-      //   state.results.splice(id, 1);
-      //   return {
-      //     ...state,
-      //     results: newArray
-      //   };
-      console.log("Delete Result fired!");
-      const updatedArray = state.results.filter(
-        result => result.id !== action.resultElId
-      );
-      return {
-        ...state,
-        results: updatedArray
-      };
+      return deleteResult(state, action);
   }
 
   return state;
 };
 
 export default reducer;
+
+/// *********************************////
+
+// case actionTypes.DELETE_RESULT:
+//   const id = 2;
+//   // here, elements if they are object, the objects themselves
+//   // are pointing the same object, but deleting is okay
+//   const newArray = [...state.results];
+//   state.results.splice(id, 1);
+//   return {
+//     ...state,
+//     results: newArray
+//   };
